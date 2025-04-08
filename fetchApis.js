679c2537cecd0224cce6c9524cc4a94ac263697e
Trell_ie <149860492+TrellTrell@users.noreply.github.com>
2025-04-08 15:54:28 +1200

    Update fetchApis.js

--- fetchApis.js
+++ fetchApis.js
@@ -3,6 +3,7 @@ const fs = require('fs-extra');
 const path = require('path');
 
 const API_TOKEN = process.env.API_TOKEN;
+const API_PASSWORD = process.env.API_PASSWORD;
 
 if (!API_TOKEN) {
   console.error('API token is missing. Set it as a GitHub secret.');
@@ -11,17 +12,17 @@ if (!API_TOKEN) {
 
 // List of APIs to fetch
 const apis = [
-    { name: 'collectibles-categories', url: 'https://shop-archives-api.vercel.app/api/collectibles-categories' },
-    { name: 'consumables', url: 'https://shop-archives-api.vercel.app/api/consumables' },
-    { name: 'downloads', url: 'https://shop-archives-api.vercel.app/api/downloads' },
-    { name: 'miscellaneous-categories', url: 'https://shop-archives-api.vercel.app/api/miscellaneous-categories' },
-    { name: 'orbs-shop-default', url: 'https://shop-archives-api.vercel.app/api/orbs-shop-default' },
-    { name: 'preview-1', url: 'https://shop-archives-api.vercel.app/api/preview-1' },
-    { name: 'preview-3', url: 'https://shop-archives-api.vercel.app/api/preview-3' },
-    { name: 'profiles-plus-categories', url: 'https://shop-archives-api.vercel.app/api/profiles-plus-categories' },
-    { name: 'profiles-plus-profile-effects', url: 'https://shop-archives-api.vercel.app/api/profiles-plus-profile-effects' },
-    { name: 'quick-info', url: 'https://shop-archives-api.vercel.app/api/quick-info' },
-    { name: 'user-profile-effects', url: 'https://shop-archives-api.vercel.app/api/user-profile-effects' },
+    { name: 'collectibles-home', url: 'https://shop-archives-api.vercel.app/api/collectibles-shop?tab=home' },
+    { name: 'collectibles-shop', url: 'https://shop-archives-api.vercel.app/api/collectibles-shop?tab=shop' },
+    { name: 'collectibles-orbs', url: 'https://shop-archives-api.vercel.app/api/collectibles-shop?tab=orbs' },
+    { name: 'collectibles-leaks', url: 'https://shop-archives-api.vercel.app/api/collectibles-shop?tab=leaks' },
+    { name: 'collectibles-nameplates', url: 'https://shop-archives-api.vercel.app/api/collectibles-shop?tab=nameplates' },
+    { name: 'collectibles-consumables', url: 'https://shop-archives-api.vercel.app/api/collectibles-shop?tab=consumables' },
+    { name: 'collectibles-miscellaneous', url: 'https://shop-archives-api.vercel.app/api/collectibles-shop?tab=miscellaneous' },
+    { name: 'pplus-home', url: 'https://shop-archives-api.vercel.app/api/collectibles-shop?tab=pplus-home' },
+    { name: 'pplus', url: 'https://shop-archives-api.vercel.app/api/collectibles-shop?tab=pplus' },
+    { name: 'profile-effects-discord', url: 'https://shop-archives-api.vercel.app/api/profile-effects?tab=discord' },
+    { name: 'profile-effects-pplus', url: 'https://shop-archives-api.vercel.app/api/profile-effects?tab=pplus' },
 ];
 
 async function fetchData() {
@@ -35,6 +36,7 @@ async function fetchData() {
       const response = await axios.get(api.url, {
         headers: {
           token: API_TOKEN,
+          password: API_PASSWORD,
           'Content-Type': 'application/json'
         }
       });
